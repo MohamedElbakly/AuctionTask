019534f1cd6354342be62c4295c56a751b76473a
Mohamed Elbakly <melbakly@Netways.com>
2021-10-23 15:19:46 +0200

    Add Generic Classes

---
+++ Auction.Business/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction.Business.Repositories.Interfaces
+{
+    public interface IUnitOfWork<out TContext>
+        where TContext : DbContext, new()
+    {
+        TContext Context { get; }
+        void CreateTransaction();
+        void Commit();
+        void Rollback();
+        void Save();
+    }
+}
